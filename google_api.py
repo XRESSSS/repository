e204230766a7618b6c8591cbebe366513c970d67
evgen <evgenijfucenko2@gmail.com>
2023-09-05 19:39:59 +0300

    api1

---
+++ google_api.py
@@ -0,0 +1,33 @@
+import requests
+
+
+url = 'https://script.google.com/macros/s/AKfycbzZ7ofoaQy2pbc7u-IjT71TxscX2GUd5iv7H4EVbXIR6ml32HsBVcG5AXTRPk1rleqGXw/exec'
+
+data = requests.get(url)
+
+data_dict = data.json()
+
+money = 0
+
+names = []
+# for person in data_dict['trip']:
+#     if person['money']:
+#         print(f'{person["name"]} -->> {person["money"]}')
+#         money += person['money']
+#
+#     names.append(person["name"])
+#
+# print(money)
+
+money = [person["money"] for person in data_dict['trip']]
+money = sum([person["money"] for person in data_dict['trip'] if person['money']])
+print(money)
+
+names = {person for person in data_dict['trip']}
+print(names)
+
+names = [person for person in data_dict['trip']]
+print(names)
+
+names = {person["name"]: person for person in data_dict['trip']}
+print(names)
