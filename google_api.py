65d611359bc12ee3512c939e2b0a46f0983c2460
evgen <evgenijfucenko2@gmail.com>
2023-09-05 20:01:48 +0300

    api1

--- google_api.py
+++ google_api.py
@@ -1,5 +1,7 @@
-import requests
+import sys
 
+import requests
+import inspect
 
 url = 'https://script.google.com/macros/s/AKfycbzZ7ofoaQy2pbc7u-IjT71TxscX2GUd5iv7H4EVbXIR6ml32HsBVcG5AXTRPk1rleqGXw/exec'
 
@@ -19,15 +21,24 @@ names = []
 #
 # print(money)
 
-money = [person["money"] for person in data_dict['trip']]
-money = sum([person["money"] for person in data_dict['trip'] if person['money']])
-print(money)
+# money = [person["money"] for person in data_dict['trip']]
+# money = sum([person["money"] for person in data_dict['trip'] if person['money']])
+# print(money)
+#
+# names = {person for person in data_dict['trip']}
+# print(names)
+#
+# names = [person for person in data_dict['trip']]
+# print(names)
+#
+# names = {person["name"]: person for person in data_dict['trip']}
+# print(names)
 
-names = {person for person in data_dict['trip']}
+names = (person for person in data_dict['trip'])
 print(names)
+print(sys.getsizeof(names))
+
 
 names = [person for person in data_dict['trip']]
 print(names)
-
-names = {person["name"]: person for person in data_dict['trip']}
-print(names)
+print(sys.getsizeof(names))
